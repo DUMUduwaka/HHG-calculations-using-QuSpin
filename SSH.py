f0a817fb475d37be2732196fe2049ac1b5c13724
DUMUduwaka <mihirangausitha@gmail.com>
2024-11-14 00:24:52 -0500

    Changed hoping parameters

--- SSH.py
+++ SSH.py
@@ -1,35 +1,36 @@
-from quspin.operators import hamiltonian  # Hamiltonians and operators
+from quspin.operators import hamiltonian            # Hamiltonians and operators
 from quspin.basis import spinless_fermion_basis_1d  # Hilbert space fermion basis
-from quspin.tools.measurements import obs_vs_time
+from quspin.tools.measurements import obs_vs_time   # Tools for measurements
 import scipy as sp
-import numpy as np  # generic math functions
+import numpy as np  
 import matplotlib.pyplot as plt
 
 
 # Define model parameters
-L = 100               # system size
-J = 1.0              # uniform hopping contribution
-a = 2.0              # Lattice constant in a.u
-delta = 0.15         # Alternating shift of the atos casuing the dimerization (negative for topological phase)
+L = 100           # system size
+J = 1.0           # uniform hopping contribution
+a = 2.0           # Lattice constant in a.u
+delta = 0.15      # Alternating shift of the atos casuing the dimerization (negative for topological phase)
 
 
 # Declare constants for Vector Potential
-NN = 5                      # Period of the pulse
-omega_0 = 0.10865675         # Frequency in THz
-F_0 = 0.2                   # Amplitude 
-tf = 300  # Final time
-t_0 = 0.52                  # Intra chain hopping parameter in eV
+NN = 5                               # Period of the pulse
+omega_0 = 0.0075                     # Frequency in THz
+F_0 = 0.2                            # Amplitude 
+tf = 2*np.pi*NN/omega_0              # Final time
+t_conversion = 2.4188843265864e-2    # Conversion of time from a.u to fs
 
 
 # Define atomic-site positions
 positions = np.zeros(L)
-
 for i in range(L):
     positions[i]=((i+1)-((L+1)/2))*a-((-1)**(i+1))*delta
+
 
 # Define the hopping elements using the distances between atoms
 v = -np.exp(-(a-2*delta)) # intracell hopping parameter
 w = -np.exp(-(a+2*delta)) # intercell hopping parameter
+
 
 # Define site-coupling lists
 hop_pm = []
@@ -44,35 +45,36 @@ for i in range(L):
         hop_mp = hop_mp + [[w, i, (i + 1) % L]]
 
 
+# Define the time arrayn and the Vector potential 
 start,stop,num = 0, tf, 250  # time in fs
-t = np.linspace(start, stop, num=num, endpoint=False)      # Time array
-A_t = F_0*((np.sin(omega_0*t/(2*NN)))**2)*np.sin(omega_0*t)     # Vector Potential
+t = np.linspace(start, stop, num=num, endpoint=False)         # Time array
+A_t = F_0*((np.sin(omega_0*t/(2*NN)))**2)*np.sin(omega_0*t)   # Vector Potential
 
 
-
-# Plot Vector potential; A(t)
-plt.plot(t,A_t)
+# Plot Vector potential A(t)
+plt.plot(t*t_conversion,A_t)
+plt.title("Vector Potential vs Time(fs)")
 plt.xlabel("time (fs)")
 plt.ylabel("Vector Potential A(t)")
 plt.show()
 
-'''
+# Define time dependent part in the Hamiltonian
 
-def ramp(t,F_0,omega_0,NN,a):
-    A_t = (F_0*10/omega_0)*((np.sin(omega_0*t/(2*NN)))**2)*np.sin(omega_0*t)
+def ramp(t,F_0,omega_0,NN):
+    A_t = F_0*((np.sin(omega_0*t/(2*NN)))**2)*np.sin(omega_0*t) 
     return np.exp(-1j*A_t)
 
-def ramp_conj(t,F_0,omega_0,NN,a):
-    A_t = (F_0*10/omega_0)*((np.sin(omega_0*t/(2*NN)))**2)*np.sin(omega_0*t)
+def ramp_conj(t,F_0,omega_0,NN):
+    A_t = F_0*((np.sin(omega_0*t/(2*NN)))**2)*np.sin(omega_0*t) 
     return np.exp(1j*A_t)
 
 ramp_args = [F_0,omega_0,NN,a]
 
-'''
+
 print(ramp_args)
 plt.plot(t,ramp(t,F_0,omega_0,NN,a))
 plt.show()
-'''
+
 
 ## Construct single-particle Hamiltonian 
 
@@ -154,9 +156,6 @@ plt.plot(omega,np.abs(A_t_omega)**2)
 plt.show()
 
 
-
-
-
 # Plot J(t) and omega * J(omega) (magnitude)
 plt.figure()
 plt.plot(omega,S_omega)
@@ -169,12 +168,11 @@ J_omega_W = (delta_t/(np.sqrt(2*np.pi)))*N*sp.fft.fft(current_time*W)
 omega_J_omega_W = omega * J_omega_W
 S_omega_W = abs(omega_J_omega_W)**2
 
-'''
-'''
+
 # Plot J(t) and omega * J(omega) (magnitude)
 plt.figure()
 plt.plot(S_omega_W)
 plt.yscale('log')
 plt.ylabel("S(Omega)_W")
 plt.show()
-'''
+
